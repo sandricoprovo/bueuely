0dc2d80bdcb5d65e90c05773590b5b1e54140881
Sandrico Provo <Sprovo@outlook.com>
2023-06-10 07:01:46 -0300

    feat(ui): adds color scales to css root

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,12 @@
 <script lang="ts">
-    const helloworld = 'Hello World';
+    const helloworld = "Hello World";
+    console.log(helloworld);
 </script>
 
 <h1>HOME PAGE</h1>
 
 <style lang="postcss">
+    h1 {
+        color: red;
+    }
 </style>
